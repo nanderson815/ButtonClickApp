0f3ca86575e49d429092b3eec854b9fe6dcbad13
Noah A <nanderson815@gmail.com>
2019-06-15 20:15:51 -0400

    adding logs

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.buttonclickapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Button
 import android.widget.TextView
 
+private val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Log.d(TAG, "onCreate: called")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -25,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
+                Log.d(TAG, "onClick: Called")
                 textView?.append(userInput.text)
                 textView?.append("\n")
                 userInput.setText("")
